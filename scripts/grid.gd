68b7c442accaae13b9879f2c22f7c2f803892610
Kelvin Huang <kh3427@drexel.edu>
2024-02-06 13:41:04 -0500

    Test push

---
+++ scripts/grid.gd
@@ -0,0 +1,15 @@
+extends Node
+
+@export var tile_scene: PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+	#for i in range(10):
+	#	var row = .new()
+	#	var tile = tile_scene.instantiate()
+	#	add_child(tile)
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
