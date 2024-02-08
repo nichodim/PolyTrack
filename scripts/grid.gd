ef6103f8621d7f591fabe1a749058a936a718d9d
Kelvin Huang <kh3427@drexel.edu>
2024-02-08 11:12:42 -0500

    Pull

--- scripts/grid.gd
+++ scripts/grid.gd
@@ -2,13 +2,24 @@ extends Node
 
 @export var tile_scene: PackedScene
 
+@export var columns = 1150/50 + 1
+@export var rows = 8
+@export var tile_size = 50
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
-	#for i in range(10):
-	#	var row = .new()
-	#	var tile = tile_scene.instantiate()
-	#	add_child(tile)
+	var grid = GridContainer.new()
+	grid.columns = columns
+	
+	grid.position = Vector2(1.0, 0.0)
+	grid.add_theme_constant_override("h_separation", 0)
+	grid.add_theme_constant_override("v_separation", 0)
+	
+	for i in range(columns * rows):
+		var tile = tile_scene.instantiate()
+		tile.size = Vector2(100.0, 100.0)
+		tile.child_entered_tree
+		grid.add_child(tile)
+	add_child(grid)
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
