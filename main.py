ba6894696b05bb77b5058304db8413b488b0dfe3
nichodim <nas374@drexel.edu>
2024-02-11 00:07:24 -0500

    Created the data system between the board and tracks

--- main.py
+++ main.py
@@ -35,38 +35,45 @@ class Colors:
     dark_gray = (40, 40, 40)
     light_gray = (70,70,70)
 
-# Creates the 2d list for row and col with inital val of 0
+
+# Creates a board rectangle where tiles will be placed
 board_width = NUM_COLS * TRACK_WIDTH + INNER_GAP * (NUM_COLS - 1) + OUTER_GAP * 2
 board_height = NUM_ROWS * TRACK_HEIGHT + INNER_GAP * (NUM_ROWS - 1) + OUTER_GAP * 2
 board_x = GAME_WIDTH / 2 - board_width / 2
 board = pygame.Rect(board_x, GAME_HEIGHT * 0.1, board_width, board_height)
 
+# Creates a 2d list with all of the tiles
+# All tiles are preset with their correct position respective to the board
+# Each tile is a list with 2 values
+#       - The board rectangle itself
+#       - The track rectangle that may attach to it
 grid_tiles = []
 row_y_increment = TRACK_HEIGHT + INNER_GAP
 row_y = board.top + OUTER_GAP
-                # Row       # Col
-# i.e. grid = [[0,0,0,0], [0,0,0,0,] ...]
 for row in range(NUM_ROWS):  
     tile_x = OUTER_GAP
 
     row_list = []
     for col in range(NUM_COLS): 
         board_tile = pygame.Rect(board.left + tile_x, row_y, TRACK_WIDTH, TRACK_HEIGHT)
-        row_list.append(board_tile)
+        row_list.append({"board": board_tile, "track": None})
 
         tile_x += TRACK_WIDTH + INNER_GAP
     grid_tiles.append(row_list)    
 
     row_y += row_y_increment 
 
+
 # Creates the boxes to drag and drop
 extra_width = TRACK_WIDTH * 0.75
 extra_height = TRACK_WIDTH / 2
 
+# Creates track box underneath the tracks
 track_box_width = NUMBER_OF_TRACKS * TRACK_WIDTH + (TRACK_SEPERATION - TRACK_WIDTH) * (NUMBER_OF_TRACKS - 1) + extra_width * 4
 track_box_x = GAME_WIDTH / 2 - track_box_width / 2
 track_box = pygame.Rect(track_box_x, GAME_HEIGHT * 0.85, track_box_width, TRACK_HEIGHT + extra_height * 2)
 
+# Creates tracks respective to the toolbox
 active_track = None
 tracks = []
 track_x = extra_width * 2
@@ -90,12 +97,14 @@ while True:
         
         if event.type == pygame.MOUSEBUTTONUP: # Checks for left mouse button releases on boxes
             if event.button == 1:
-                # Check if pointer over a board tile
+                # Check if pointer over an empty board tile
+                #   -> Snap track to board tile and link track to tile
                 for i, row in enumerate(grid_tiles):
                     for j, tile in enumerate(row):
-                        if tile.collidepoint(event.pos):
-                            tracks[active_track].x = grid_tiles[i][j].left
-                            tracks[active_track].y = grid_tiles[i][j].top
+                        if tile['board'].collidepoint(event.pos) and not grid_tiles[i][j]['track']:
+                            tracks[active_track].x = grid_tiles[i][j]['board'].left
+                            tracks[active_track].y = grid_tiles[i][j]['board'].top
+                            grid_tiles[i][j]['track'] = tracks[active_track]
 
                 active_track = None
         
@@ -114,7 +123,7 @@ while True:
     pygame.draw.rect(game_surf, Colors.light_gray, board)
     for row in grid_tiles:
         for tile in row:
-            pygame.draw.rect(game_surf, "white", tile)
+            pygame.draw.rect(game_surf, "white", tile['board'])
 
     pygame.draw.rect(game_surf, "black", track_box)
     # Draws the tracks on the screen
